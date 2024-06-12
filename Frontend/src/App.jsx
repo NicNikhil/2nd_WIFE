42ecf16060d7e0a1cba10331b32bc138f02527e0
Nic_Nikhil <nikhilsaraswat57@gmail.com>
2024-06-12 18:42:26 +0530

    updating frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,18 +4,24 @@ import { Route, Routes } from 'react-router-dom'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
+import Fooddisplay from './components/FoodDisplay/Fooddisplay'
+import Footer from './components/Footer/Footer'
 const App = () => {
   return (
-    <div className='app'>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/Cart' element={<Cart />} />
-        <Route path='/PlaceOrder' element={<PlaceOrder />} />
+    <>
+      <div className='app'>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/Cart' element={<Cart />} />
+          <Route path='/PlaceOrder' element={<PlaceOrder />} />
 
 
-      </Routes>
-    </div>
+        </Routes>
+      </div>
+      <Footer />
+    </>
+
   )
 }
 
--- Frontend/src/context/StoreContext.jsx
+++ Frontend/src/context/StoreContext.jsx
@@ -1,11 +1,10 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { food_list } from "../assets/frontend_assets/assets";
 
 
 export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props) => {
-
 
     const [cartItems, setcartItems] = useState({});
 
@@ -18,11 +17,18 @@ const StoreContextProvider = (props) => {
         }
     }
     const removeFromCart = (itemId) => {
-
+        setcartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
     }
+    useEffect(() => {
+        console.log(cartItems);
+    }, [cartItems])
 
     const contextValue = {
-        food_list
+        food_list,
+        cartItems,
+        setcartItems,
+        addToCart,
+        removeFromCart
     }
     return (
         <StoreContext.Provider value={contextValue}>
