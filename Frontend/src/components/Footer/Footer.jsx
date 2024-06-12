dcc92b3be678586af4effedc4b841de2dd3260e8
Nic_Nikhil <nikhilsaraswat57@gmail.com>
2024-06-12 19:57:20 +0530

    ..

--- Frontend/src/components/Footer/Footer.jsx
+++ Frontend/src/components/Footer/Footer.jsx
@@ -7,17 +7,36 @@ const Footer = () => {
         <div className='footer' id='footer'>
             <div className="footer-content">
                 <div className="footer content left">
-                    <img src={assets.logo} alt='' />
-                    <p></p>
+                    <img src={assets.} alt='' />
+                    <p>Follow Us on Social Media for the Latest Updates and Special Offers!</p>
+                    <div className="footer social icons">
+                        <img src={assets.facebook_icon} alt='' />
+                        <img src={assets.twitter_icon} alt='' />
+                        <img src={assets.linkedin_icon} alt='' />
+
+                    </div>
                 </div>
                 <div className="footer content center">
+                    <h2>COMPANY</h2>
+                    <ul>
+                        <li>Home</li>
+                        <li>About us</li>
+                        <li>Delivery</li>
+                        <li>Privacy policy</li>
 
+                    </ul>
                 </div>
                 <div className="footer content right">
-
+                    <h2>GET IN TOUCH</h2>
+                    <ul>
+                        <li> +91-9999999999
+                        </li>
+                        <li>contact@2ndwife.com</li>
+                    </ul>
                 </div>
             </div>
-
+            <hr />
+            <p className='footer-copyright'>Copyright 2024 © 2ndwife.com - All Right Reserved</p>
         </div>
     )
 }
